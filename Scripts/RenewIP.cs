9d826cbffe699e97a7b470b871b8f1fe3a2c6782
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-06-12 10:33:27 +0200

    Update RenewIP.cs

--- Scripts/RenewIP.cs
+++ Scripts/RenewIP.cs
@@ -1,1 +1,45 @@
+using System;
+using System.Diagnostics;
 
+class IPAddressRenewalScript
+{
+    static void Main()
+    {
+        ReleaseIPAddress();
+        RenewIPAddress();
+
+        Console.WriteLine("IP address has been released and renewed.");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void ReleaseIPAddress()
+    {
+        ExecuteCommand("ipconfig /release");
+    }
+
+    static void RenewIPAddress()
+    {
+        ExecuteCommand("ipconfig /renew");
+    }
+
+    static void ExecuteCommand(string command)
+    {
+        ProcessStartInfo processInfo = new ProcessStartInfo();
+        processInfo.FileName = "cmd.exe";
+        processInfo.Arguments = "/c " + command;
+        processInfo.RedirectStandardOutput = true;
+        processInfo.UseShellExecute = false;
+        processInfo.CreateNoWindow = true;
+
+        Process process = new Process();
+        process.StartInfo = processInfo;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        Console.WriteLine(output);
+
+        process.WaitForExit();
+    }
+}
